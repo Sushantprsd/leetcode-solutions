f861c1bd6d0583bc4cd590f6c445490518353961
SUSHANT PRASAD <43819292+Sushantprsd@users.noreply.github.com>
2026-09-11 12:53:26 +0530

    Time: 8 ms (77.34%) | Memory: 19.3 MB (45.08%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int ans = 0;
-        int start = 0;
-        unordered_map<char, int> dict;
+  int lengthOfLongestSubstring(string s) {
+    int ans = 0;
+    int currentLength = 0;
+    int start = 0;
+    vector<int> dict(256, -1);
 
-        for (int i = 0; i < s.length(); i++) {
-            if (dict.find(s[i]) != dict.end()) {
-                start = max(start, dict[s[i]] + 1);
-            }
-
-            dict[s[i]] = i;
-            ans = max(ans, i - start + 1);
-        }
-
-        return ans;
+    for (int i = 0; i < s.length(); i++) {
+      if (dict[s[i]] > -1 && dict[s[i]] < start) {
+      } else  if (dict[s[i]] > -1 ){
+        start = dict[s[i]] + 1;
+      }
+      dict[s[i]] = i;
+      ans = max(ans, i - start + 1);
     }
+    return ans;
+  }
 };
