d57fee3c330e0dc9edcf193d7311076ff7e40296
SUSHANT PRASAD <43819292+Sushantprsd@users.noreply.github.com>
2026-09-17 15:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (60.78%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,34 +3,37 @@ public:
   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     int n1 = nums1.size();
     int n2 = nums2.size();
-    if (n1 > n2)
+
+    if (n2 < n1) {
       return findMedianSortedArrays(nums2, nums1);
-    // suppose nums1.size() < nums2.size()
+    }
+
     int low = 0;
     int high = n1;
     int midPos = (n1 + n2 + 1) / 2;
-
     while (low <= high) {
-      int m1 = (low + high)/2;
-      int m2 = midPos - m1;
+      int mid1 = (low + high) / 2;
+      int mid2 = midPos - mid1;
 
-      int l1 = (m1 == 0) ? INT_MIN : nums1[m1 - 1];
-      int r1 = (m1 == n1) ? INT_MAX : nums1[m1];
+      int l1 = mid1 - 1 < 0 ? INT_MIN : nums1[mid1-1];
+      int r1 = mid1 >= nums1.size() ? INT_MAX: nums1[mid1];
 
-      int l2 = (m2 == 0) ? INT_MIN : nums2[m2 - 1];
-      int r2 = (m2 == n2) ? INT_MAX : nums2[m2]; 
-      if (l1 <= r2 && r1 >= l2) {
-        if ((n1 + n2) % 2 == 0) {
-          return ((double)max(l1, l2) + (double)min(r1, r2)) / 2.0;
+      int l2 = mid2 - 1 < 0 ? INT_MIN: nums2[mid2-1];
+      int r2 = mid2 >= nums2.size() ?  INT_MAX: nums2[mid2];
+
+      if (l1 > r2) {
+        high = mid1 - 1;
+      } else if (l2 > r1) {
+        low = mid1 + 1;
+      } else {
+        if ((n1+n2)%2 == 0) {
+          return double((max(l1, l2) + min(r1,r2))/ 2.0);
         } else {
           return max(l1, l2);
         }
-      } else if (l1 > r2) {
-        high = m1 - 1;
-      } else {
-        low = m1 + 1;
       }
     }
     return 0;
+
   }
 };
