7bfa95aab59db223f27a15a962a38140b5515617
SUSHANT PRASAD <43819292+Sushantprsd@users.noreply.github.com>
2026-09-17 15:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (60.78%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+    if (n1 > n2)
+      return findMedianSortedArrays(nums2, nums1);
+    // suppose nums1.size() < nums2.size()
+    int low = 0;
+    int high = n1;
+    int midPos = (n1 + n2 + 1) / 2;
 
-        // Always binary search the smaller array
-        if (n1 > n2) {
-            return findMedianSortedArrays(nums2, nums1);
+    while (low <= high) {
+      int m1 = (low + high)/2;
+      int m2 = midPos - m1;
+
+      int l1 = (m1 == 0) ? INT_MIN : nums1[m1 - 1];
+      int r1 = (m1 == n1) ? INT_MAX : nums1[m1];
+
+      int l2 = (m2 == 0) ? INT_MIN : nums2[m2 - 1];
+      int r2 = (m2 == n2) ? INT_MAX : nums2[m2]; 
+      if (l1 <= r2 && r1 >= l2) {
+        if ((n1 + n2) % 2 == 0) {
+          return ((double)max(l1, l2) + (double)min(r1, r2)) / 2.0;
+        } else {
+          return max(l1, l2);
         }
-
-        int total = n1 + n2;
-        int half = (total + 1) / 2;
-
-        int low = max(0, half - n2);
-        int high = min(n1, half);
-
-        while (low <= high) {
-            int mid1 = low + (high - low) / 2;
-            int mid2 = half - mid1;
-
-            int l1 = (mid1 == 0) ? INT_MIN : nums1[mid1 - 1];
-            int r1 = (mid1 == n1) ? INT_MAX : nums1[mid1];
-
-            int l2 = (mid2 == 0) ? INT_MIN : nums2[mid2 - 1];
-            int r2 = (mid2 == n2) ? INT_MAX : nums2[mid2];
-
-            if (l1 > r2) {
-                high = mid1 - 1;
-            }
-            else if (l2 > r1) {
-                low = mid1 + 1;
-            }
-            else {
-                if (total % 2 == 0) {
-                    return (max(l1, l2) + min(r1, r2)) / 2.0;
-                }
-
-                return max(l1, l2);
-            }
-        }
-
-        return 0.0;
+      } else if (l1 > r2) {
+        high = m1 - 1;
+      } else {
+        low = m1 + 1;
+      }
     }
+    return 0;
+  }
 };
